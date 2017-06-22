61817701a764b23ecc03174207858184d13c7164
Jordan Phillips <jjplayer1127@gmail.com>
2017-06-22 19:02:56 -0400

    setting up

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,4 @@
 //Meteor.publish("Numbers", function(){return Numbers.find();});
 //Meteor.publish("Users", function(){return Users.find();});
 Meteor.publish("Cards", function(){return Cards.find().fetch()})
+Meteor.publish("Games", function(){return Games.find().fetch()})
