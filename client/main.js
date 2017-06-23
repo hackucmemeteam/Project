98d40d0fe51c679732b10a8604331a0f91cab693
Jordan Phillips <jjplayer1127@gmail.com>
2017-06-22 21:51:54 -0400

    whole page

--- client/main.js
+++ client/main.js
@@ -1,36 +1,11 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+Template.home.rendered = function() {
+  $('html, body').css({
+    "background-image": "url('http://i.imgur.com/u7AO4rY.png')",
+  })
+};
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return 11;
-  }
-});
-Template.usernames.helpers({
-	Users:function(){
-  	return Users.find().fetch()
-  }
-})
-Template.usernames.events({
-	'click #usernametext'(event,instance){
-		Users.remove({_id:this._id});
-	}
-})
-Template.hello.events({
-  'click #clickButton'(event, instance) {
-    // increment the counter when button is clicked
-
-    instance.counter.set(instance.counter.get() + 1);
-
-    var input = $('#userInput').val();
-    Users.insert({username:input});
-    $('#userInput').val("");
-  },
-});
+Template.home.destroyed = function() {
+  $('html, body').css({
+    "background-image": "none"
+  })
+};
